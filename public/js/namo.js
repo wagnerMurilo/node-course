e03f8db17d8d80e2cf8fd2433d8c5e425a773a1c
wagnerMurilo <wagner_murilo_machado@yahoo.com.br>
2019-11-04 11:52:02 +0100

    case sensitive issue

--- public/js/namo.js
+++ public/js/namo.js
@@ -17,23 +17,23 @@ namo.addEventListener('submit', (e) => {
         {
             namoMsg1.textContent = 'O seu namorado e o Bernardo'    
             namoMsg2.textContent = 'Ele e romantigo, gosta de cavalgadas no parque, cinefilo e zoofilo, tem musculos definidos e coloca chantily no imbigo pra vc lambe'
-            namoImg.src ='img/Namo1.jpg'
+            namoImg.src ='img/namo1.jpg'
 
         }else if(0.4>randomNamo>0.2)
         {
             namoMsg1.textContent = 'O seu namorado e o Breno'   
             namoMsg2.textContent = 'Ele e corajoso como um tigre, forte como um lobo, voraz como uma hiena. Ele tb e esperto como uma lebre. Pessoas dizem q ele tb e astuto como um leopardo e agil como um gorila. A mae dele fala q ele e forte como um chipanze. Ele e modesto como um avestruz e diz apenas trabalhar duro como um rinoceronte'
-            namoImg.src ='img/Namo2.jpg'
+            namoImg.src ='img/namo2.jpg'
         }else if(0.6>randomNamo>0.4)
         {
             namoMsg1.textContent = 'Seu namorado e o Jurandir'    
             namoMsg2.textContent = 'Pau pra toda obra. Otimo pai, construiu uma casa para as filhas com as proprias maos, onde mantem elas trancadas. Ele tambem e pintor, motoboy, medico em medicina e artezao, mas perdeu o ar quando te viu e ficou com tezao.'
-            namoImg.src ='img/Namo3.jpg'
+            namoImg.src ='img/namo3.jpg'
         }else if(0.8>randomNamo>0.6)
         {
             namoMsg1.textContent = 'Seu namorado e o Samuel'    
             namoMsg2.textContent = 'Gamer, geek, programador full stack, tem um podcast e e fullrange. Seu super heroi favorito e o Pantera Verde e e fa de star trek(guerra nas estrelas)'
-            namoImg.src ='img/Namo4.jpg'
+            namoImg.src ='img/namo4.jpg'
         }
         else
         {
--- public/js/news.js
+++ public/js/news.js
@@ -16,24 +16,24 @@ fetch('/news1234').then((response) => {
         if(randomNews<0.2)
         {
             newsMessage2.textContent = 'Jornalista do jornal direitaonline.org'    
-            newsImage.src ='img/News1.jpg'
+            newsImage.src ='img/news1.jpg'
         }else if(0.4>randomNews>0.2)
         {
             newsMessage2.textContent = 'Colunista do jornal folha de'   
-            newsImage.src ='img/News2.jpg'
+            newsImage.src ='img/news2.jpg'
         }else if(0.6>randomNews>0.4)
         {
             newsMessage2.textContent = 'Jornalista do jornal DemiLovatoNews'    
-            newsImage.src ='img/News3.jpg'
+            newsImage.src ='img/news3.jpg'
         }else if(0.8>randomNews>0.6)
         {
             newsMessage2.textContent = 'Estudante'
-            newsImage.src ='img/News4.jpg'
+            newsImage.src ='img/news4.jpg'
         }
         else
         {
             newsMessage2.textContent = 'Rei da Belgica'
-            newsImage.src ='img/News5.jpg'
+            newsImage.src ='img/news5.jpg'
         }
     
         console.log(data)
